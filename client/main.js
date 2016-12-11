d634e79e9235df874f0a4c3ecbb0c04654e5b7e5
Andy Eccleston <eandy5000@yahoo.com>
2016-12-11 13:03:58 -0600

    mid turning app into a class based component

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 'use strict'
 //libraries
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 import axios from 'axios'
 
@@ -8,18 +8,36 @@ import axios from 'axios'
 import ImageList from './components/image_list'
 
 
+
 //component
-const App = ()=> {
-    return (
-        <div>
-            <h1>Image List</h1>
-            <ImageList />
-        </div>
-    )
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            images: []
+        }
+    }
+
+    componentWillMount(){
+        axios
+            .get("https://api.imgur.com/3/gallery/hot/viral/0")
+            .then(res => console.log(res))
+    }
+    
+    render() {
+            return (
+            <div>
+                <h1>Image List</h1>
+                <ImageList />
+            </div>
+        )
+    }
 }
 
 //need this to delay js until page loads
 Meteor.startup(() => {
     ReactDom.render(<App />, document.querySelector("#container"))
+
 })
 
